55db55bb857d6064f2c4809a78ac32c86bb6deb1
Suvendu Panda <suvendu.panda@ibm.com>
2022-07-13 17:00:46 +0530

    main file added

---
+++ main.tf
@@ -0,0 +1,43 @@
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.52.0"
+      }
+    }
+
+  backend "azurerm" {
+    resource_group_name   = "TF-RG"
+    storage_account_name  = "staeustf01"
+    container_name        = "tstate"
+    key                   = "terraform.tfstate"
+    }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "myrg" {
+  name     = "TF-RG"
+  location = "eastus"
+}
+
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "myvnet" {
+  name                = "TF-VNET"
+  resource_group_name = azurerm_resource_group.myrg.name
+  location            = azurerm_resource_group.myrg.location
+  address_space       = ["10.0.0.0/16"]
+}
+
+# Create a subnet within the vnet
+resource "azurerm_subnet" "mysubnet" {
+  name                 = "TF-Subnet"
+  resource_group_name  = azurerm_resource_group.myrg.name
+  virtual_network_name = azurerm_virtual_network.myvnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
